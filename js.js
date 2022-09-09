ac2863a338e68c94e5efb09cfa33e7ecdb59b175
Mohamad Nassar <mnassar57@gmail.com>
2022-09-10 00:36:42 +0300

    added refresh feature and more styling

--- js.js
+++ js.js
@@ -1,3 +1,15 @@
+//Error message handling
+
+
+//Refresh dog image
+let refresh_icon = document.getElementById("refresh");
+refresh_icon.addEventListener("click", refreshDogImage);
+
+function refreshDogImage(){
+    console.log("gg");
+    renderDogImageURL('https://dog.ceo/api/breeds/image/random');
+} 
+
 async function fetchJSONData(url){
     try{
         let resultFromPage = await fetch(url);
@@ -22,8 +34,13 @@ async function renderGenderPrediction(url) {
 
 async function renderNationalityPrediction(url) {
     let results = await fetchJSONData(url);
-    let imageURL = results.country[0];
+    let imageURL = results.country[0].country_id;
     console.log(imageURL);
+    imageURL = results.country[1].country_id;
+    console.log(imageURL);
+    imageURL = results.country[2].country_id;
+    console.log(imageURL);
+
 }
 
 //Get dog image from URL
@@ -34,15 +51,26 @@ let n = document.getElementById("submit").addEventListener("click", submitButton
 
 function submitButton(){
     let user_name = document.getElementById("name").value;
-    console.log(user_name);
-    url = "https://api.genderize.io/?name=" + user_name;
-    console.log(url);
-    renderGenderPrediction(url);
+    let exclamation_circle = document.getElementById("exclamation-circle");
+    let main_container = document.getElementById("container");
+    
+    //main_container.classList.add("hidden");
 
-    //nationality
-    url2 = 'https://api.nationalize.io/?name=' + user_name;
-    console.log(url2);
-    renderNationalityPrediction(url2);
+    if(user_name == ''){
+        console.log("gg");
+    }
+    else{
+        console.log(user_name);
+        url = "https://api.genderize.io/?name=" + user_name;
+        console.log(url);
+        renderGenderPrediction(url);
+    
+        //nationality
+        url2 = 'https://api.nationalize.io/?name=' + user_name;
+        console.log(url2);
+        renderNationalityPrediction(url2);
+    }
+
 
 }
 
