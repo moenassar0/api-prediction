9615fcef559fdd0a4cd8b93f9b60a4ad0e741e37
Mohamad Nassar <mnassar57@gmail.com>
2022-09-11 22:56:46 +0300

    login js code added -bonus-

--- login-register.js
+++ login-register.js
@@ -1,5 +1,7 @@
 document.getElementById("register").addEventListener("click", registerButton);
-
+document.getElementById("login").addEventListener("click", loginButton);
+const username_log = document.getElementById("username-login");
+const password_log = document.getElementById("password-login");
 const username_reg = document.getElementById("username-register");
 const password_reg = document.getElementById("password-register");
 
@@ -16,4 +18,15 @@ function registerButton(){
     var json = JSON.stringify(userinfo);
     localStorage.setItem(username_reg.value + " info", json);
 }
+
+function loginButton(){
+    console.log(username_log.value);
+    console.log(password_log.value);
+
+    let userinfo = localStorage.getItem(username_log.value + " info");
+    console.log(userinfo);
+    let jsoninfo = JSON.parse(userinfo);
+    console.log(jsoninfo)
+}
+
 
